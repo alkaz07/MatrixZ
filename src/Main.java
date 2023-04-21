d5e3fbe7815259b857df9062862be82de425853e
alkaz07 <alkaz07@yandex.ru>
2023-04-21 19:35:07 +0300

    fixed: j<matrix[i].length

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Random;
 
 public class Main {
+
+    static final int N = 10;
     public static void main(String[] args) {
-        int[][] matr = new int[10][10];
+        int[][] matr = new int[N][N];
         fillRandomMatrix(matr);
         printMatrix(matr);
 
@@ -11,16 +13,16 @@ public class Main {
     private static void fillRandomMatrix(int[][] matrix) {
         Random ran = new Random();
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = ran.nextInt(10) + 1;
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ran.nextInt(99) ;
             }
         }
     }
 
     public static void printMatrix( int matrix[][]) {
         printStar();
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 //System.out.print(matrix[i][j]+" ");
                 System.out.printf("%3s", matrix[i][j]);
             }
