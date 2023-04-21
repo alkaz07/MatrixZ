8667770265cbb3c82586d6d95931bc71c6223b04
alkaz07 <alkaz07@yandex.ru>
2023-04-21 20:10:37 +0300

    + countRepeats

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -7,14 +8,16 @@ public class Main {
         int[][] matr = new int[N][N];
         fillRandomMatrix(matr);
         printMatrix(matr);
-
+        printStar();
+        int[] counts = countRepeatsInLines(matr);
+        System.out.println(Arrays.toString(counts));
     }
 
     private static void fillRandomMatrix(int[][] matrix) {
         Random ran = new Random();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = ran.nextInt(99) ;
+                matrix[i][j] = ran.nextInt(49) ;
             }
         }
     }
@@ -33,5 +36,20 @@ public class Main {
         System.out.println();
         System.out.println("*****************************************");
         System.out.println();
+    }
+
+    public static int[] countRepeatsInLines(int matrix[][]){
+        int[] res = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                for (int k = j+1; k < matrix[i].length; k++) {
+                    if (matrix[i][j] == matrix[i][k])
+                        res[i]++;
+                }
+            }
+        }
+
+
+        return res;
     }
 }
