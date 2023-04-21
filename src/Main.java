9f31eb052f5261f7c520db68b6fc2b6af9f177b2
alkaz07 <alkaz07@yandex.ru>
2023-04-21 19:08:45 +0300

    + random changed: printf

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int[][] matr = new int[10][10];
+        fillRandomMatrix(matr);
+        printMatrix(matr);
 
+    }
 
+    private static void fillRandomMatrix(int[][] matrix) {
+        Random ran = new Random();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = ran.nextInt(10) + 1;
+            }
+        }
     }
 
     public static void printMatrix( int matrix[][]) {
         printStar();
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.print(matrix[i][j]+" ");
+                //System.out.print(matrix[i][j]+" ");
+                System.out.printf("%3s", matrix[i][j]);
             }
             System.out.println();
         }
