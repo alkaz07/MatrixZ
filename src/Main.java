58372903427936bea46fdc8a38a1b20779028a08
alkaz07 <alkaz07@yandex.ru>
2023-04-21 20:58:56 +0300

    + sort змейкой + заготовка sort spiral

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class Main {
@@ -11,6 +13,8 @@ public class Main {
         printStar();
         int[] counts = countRepeatsInLines(matr);
         System.out.println(Arrays.toString(counts));
+
+        sortMatrix(matr);
     }
 
     private static void fillRandomMatrix(int[][] matrix) {
@@ -21,6 +25,50 @@ public class Main {
             }
         }
     }
+
+    public static void sortMatrix (int num [][]){
+        //ArrayList<Integer> array = new ArrayList<Integer>();
+        int[] arr = new int[num.length*num[0].length];
+
+        for (int i = 0, k=0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+               // array.add(num [i][j]);
+                arr[k]= num[i][j];
+                k++;
+            }
+        }
+       // Collections.sort(array);
+        Arrays.sort(arr);
+       // System.out.println(array);
+       // System.out.println(Arrays.toString(arr));
+
+//        for (int i = 0; i < array.size(); i++) {
+//            System.out.print(array.get(i) + " ");
+//        }
+//
+        int k = 0;
+        int[][] num1 = new int[10][10];
+        for (int i = 9; i >= 0 ; i--) {
+            if(i%2 == 0)
+            {
+                for (int j = 0; j < 10 ; j++) {
+                    //num1[i][j] = array.get(k);
+                    num1[i][j] = arr[k];
+                    k++;
+                }
+            }
+            else
+            {
+                for (int j = 9; j >= 0; j--) {
+                    //num1[i][j] = array.get(k);
+                    num1[i][j] = arr[k];
+                    k++;
+                }
+            }
+        }
+        printMatrix(num1);
+    }
+
 
     public static void printMatrix( int matrix[][]) {
         printStar();
@@ -48,8 +96,17 @@ public class Main {
                 }
             }
         }
-
-
+        // если такая строка матрицы, то каким должен быть  ответ?
+        // 6 27 42 42 46  3  3  3 15  7
+        // сейчас res[i] = 4
         return res;
+    }
+
+
+    public static void sortMatrixSpiral (int num [][]){
+        //построить массив из всех элементов матрицы
+        //отсортировать массив
+        //перезаполнить матрицу по спирали
+
     }
 }
